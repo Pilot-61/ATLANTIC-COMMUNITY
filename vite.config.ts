599381edcc383779f39ee0d470cd316e92c3f781
Es-salhi Mohammed Amine <117828513+Pilot-61@users.noreply.github.com>
2025-08-19 13:16:51 +0000

    path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TEST/',  // Use your repo name here!
+  base: '/ATLANTIC-COMMUNITY/',  // Use your repo name here!
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
